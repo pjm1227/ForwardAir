a9c7daf940752d331e51521dbc9d8a4f47da5a03
Rahul <rahul.bhuwan@dreamorbit.com>
2019-09-05 15:19:06 +0530

    Added fresh project structure.

---
+++ forwardair_fleet_management/android/app/src/main/java/com/forwardair/forwardair_fleet_management/MainActivity.java
@@ -0,0 +1,13 @@
+package com.forwardair.forwardair_fleet_management;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
